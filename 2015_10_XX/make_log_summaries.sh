916f5166ad402ba8592449146ad98bdfccef863d
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2015-10-14 16:49:51 +0200

    Move analyses to cartesius.

--- 2015_10_XX/make_log_summaries.sh
+++ 2015_10_XX/make_log_summaries.sh
@@ -1,5 +1,5 @@
 for i in {0..404}
 do
    echo "Processing the run $i."
-   python ../make_log.summary.py /scratch/edwin/sensitivity_analysis/2015_10_XX/code__a__$i/log/*.log summary_$i.sum
+   python ../make_log.summary.py /scratch-shared/edwin/sensitivity_analysis/2015_10_XX/code__a__$i/log/*.log summary_$i.sum
 done
