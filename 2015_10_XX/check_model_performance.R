ca3322ce0127ac289f902f2a7f971e72f18bbcc1
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-10-15 23:18:00 +0200

    Test scripts.

--- 2015_10_XX/check_model_performance.R
+++ 2015_10_XX/check_model_performance.R
@@ -43,8 +43,13 @@ river = river[which(river$num_of_month_pairs > 12), ]
 river = as.character(river$river_name[order(-river$grdc_catchment_area_in_km2)])
 
 
+# make scatter plots
+pdf("0Rtest.pdf", width=10, height=12, bg = "white")
+par(mfrow=c(4,1), mar=c(4,4,2,4))
+
 # loop through all rivers to get model performances from all runs and their corresponding model parameters
-for i_river in seq(1, length(river$name, 1)){
+#for i_river in seq(1, length(river$name), 1){
+for i_river in seq(1, 5, 1){
 
 print()
 print(i_river)
@@ -95,107 +100,13 @@ names(plot_table)[9]  <- "R2"
 names(plot_table)[10] <- "i_river"
 
 
-qplot(log_ksat, i_river, data = plot_table, size = ns_eff*1) + scale_size(range = c(0,10)); dev.off()
+qplot(log_ksat, i_river, data = plot_table, size = ns_eff*1) + 
+scale_size_area(limits = c(0, 1), breaks = seq(0,1,0.1), max_size = 10) + 
+scale_y_continuous(limits = c(i_river - 0.01, i_river + 0.01), breaks = seq(i_river-1,i_river+1,1))
  
 
 
 
 }
 
-
-
-########################################################################################################################
-# make table of average values 
-average_values = data.frame()
-
-for( i_run in seq(0,404,1)) {
-
-# open the log_summary file
-file_name = paste(main_path , as.character(i_run), ".sum", sep = "")
-print(file_name)
-
-# scenario_code
-code = paste("code__a__", as.character(i_run), sep = "")
-
-# open the log_summary file
-summary_table = read.table(file_name, sep = ";", header = T)
-
-avg_evaporation          = mean(summary_table$evaporation         )
-avg_runoff               = mean(summary_table$runoff              )
-avg_baseflow             = mean(summary_table$baseflow            )        
-avg_groundwater_recharge = mean(summary_table$groundwater_recharge)
-
-average_values = rbind(average_values, 
-                       cbind(code, 
-                             avg_evaporation,         
-                             avg_runoff,              
-                             avg_baseflow,            
-                             avg_groundwater_recharge))
-}
-
-# names of all columns
-names(average_values)[1] <- "code"
-names(average_values)[2] <- "avg_evaporation"        
-names(average_values)[3] <- "avg_runoff"             
-names(average_values)[4] <- "avg_baseflow"           
-names(average_values)[5] <- "avg_groundwater_recharge"
-
-# make sure formats are correct
-average_values[,1] <- as.character(average_values[,1])
-average_values[,2:ncol(average_values)] <- lapply(average_values[,2:ncol(average_values)], as.character)
-average_values[,2:ncol(average_values)] <- lapply(average_values[,2:ncol(average_values)], as.numeric)
-########################################################################################################################
-
-# merge two data frame
-table = merge(parameters, average_values, by = "code")
-
-# value for the reference run
-evaporation_ref = table$avg_evaporation[which(table$code == "code__a__0")]
-runoff_ref      = table$avg_runoff[which(table$code == "code__a__0")]
-baseflow_ref    = table$avg_baseflow[which(table$code == "code__a__0")]
-recharge_ref    = table$avg_groundwater_recharge[which(table$code == "code__a__0")]
-
-########################################################################################################################
-# make scatter plots
-pdf("scatter_plot.pdf", width=10, height=12, bg = "white")
-par(mfrow=c(4,4), mar=c(4,4,2,4))
-
-plot(table$min_soil_depth_frac, table$avg_evaporation)
-abline(h = evaporation_ref)
-plot(table$log_ksat           , table$avg_evaporation)
-abline(h = evaporation_ref)
-plot(table$log_recession_coef , table$avg_evaporation)
-abline(h = evaporation_ref)
-plot(table$stor_cap           , table$avg_evaporation)
-abline(h = evaporation_ref)
-
-plot(table$min_soil_depth_frac, table$avg_runoff)
-abline(h = runoff_ref)
-plot(table$log_ksat           , table$avg_runoff)
-abline(h = runoff_ref)
-plot(table$log_recession_coef , table$avg_runoff)
-abline(h = runoff_ref)
-plot(table$stor_cap           , table$avg_runoff)
-abline(h = runoff_ref)
-
-plot(table$min_soil_depth_frac, table$avg_baseflow)
-abline(h = baseflow_ref)
-plot(table$log_ksat           , table$avg_baseflow)
-abline(h = baseflow_ref)
-plot(table$log_recession_coef , table$avg_baseflow)
-abline(h = baseflow_ref)
-plot(table$stor_cap           , table$avg_baseflow)
-abline(h = baseflow_ref)
-
-plot(table$min_soil_depth_frac, table$avg_groundwater_recharge)
-abline(h = recharge_ref)
-plot(table$log_ksat           , table$avg_groundwater_recharge)
-abline(h = recharge_ref)
-plot(table$log_recession_coef , table$avg_groundwater_recharge)
-abline(h = recharge_ref)
-plot(table$stor_cap           , table$avg_groundwater_recharge)
-abline(h = recharge_ref)
-
 dev.off()
-########################################################################################################################
-
