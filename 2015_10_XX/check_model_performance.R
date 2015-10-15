17a8f36db3a8dc367537475b6d4b96ccb1fe3877
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-10-16 00:36:58 +0200

    Test scripts.

--- 2015_10_XX/check_model_performance.R
+++ 2015_10_XX/check_model_performance.R
@@ -43,16 +43,16 @@ river = as.character(river$river_name[order(-river$grdc_catchment_area_in_km2)])
 
 
 # make scatter plots
-pdf("0Rtest.pdf", width=10, height=10, bg = "white")
+pdf("0Rtest.pdf", width=10, height=50, bg = "white")
 
-#~ par(mfrow=c(length(river), 5), mar=c(4,4,2,4))
+par(mfrow=c(length(river), 5), mar=c(0,0,0,0))
 
-par(mfrow=c(10, 4), mar=c(0,0,0,0))
+#~ par(mfrow=c(10, 4), mar=c(0,0,0,0))
 
-for (i_river in seq(1, 10, 1)){
+#~ for (i_river in seq(1, 10, 1)){
 
 #~ # loop through all rivers to get model performances from all runs and their corresponding model parameters
-#~ for (i_river in seq(1, length(river), 1)) {
+for (i_river in seq(1, length(river), 1)) {
 
 
 print("")
