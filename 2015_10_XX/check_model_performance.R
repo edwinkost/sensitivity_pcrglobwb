a9c9a741ead32a75c91cbca6e3d8868048ae7320
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-10-16 00:28:43 +0200

    Test scripts.

--- 2015_10_XX/check_model_performance.R
+++ 2015_10_XX/check_model_performance.R
@@ -43,7 +43,7 @@ river = as.character(river$river_name[order(-river$grdc_catchment_area_in_km2)])
 
 
 # make scatter plots
-pdf("0Rtest.pdf", width=10, height=12, bg = "white")
+pdf("0Rtest.pdf", width=5, height=12, bg = "white")
 
 #~ par(mfrow=c(length(river), 5), mar=c(4,4,2,4))
 
