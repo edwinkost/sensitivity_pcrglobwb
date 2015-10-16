6c0d4dbfe0af72fcefcd7835c596a156b5aa571d
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-10-16 09:44:38 +0200

    Finalizing model performance script.

--- 2015_10_XX/check_model_performance.R
+++ 2015_10_XX/check_model_performance.R
@@ -90,10 +90,10 @@ R2[i_code][which(R2[i_code] < 0.0)] = 0.0
 
 }
 
-plot(parameters$min_soil_depth_frac, array(i_river, length(parameters$code)), cex = ns_eff * 5., ylim = c(i_river-0.25, i_river+0.25), yaxt = 'n', ylab = as.character(river[i_river])
-plot(parameters$log_ksat           , array(i_river, length(parameters$code)), cex = ns_eff * 5., ylim = c(i_river-0.25, i_river+0.25), yaxt = 'n', ylab = "")
-plot(parameters$log_recession_coef , array(i_river, length(parameters$code)), cex = ns_eff * 5., ylim = c(i_river-0.25, i_river+0.25), yaxt = 'n', ylab = "")
-plot(parameters$stor_cap           , array(i_river, length(parameters$code)), cex = ns_eff * 5., ylim = c(i_river-0.25, i_river+0.25), yaxt = 'n', ylab = "")
+plot(parameters$min_soil_depth_frac, array(i_river, length(parameters$code)), cex = ns_eff * 5., yaxt = 'n', ylab = as.character(river[i_river])
+plot(parameters$log_ksat           , array(i_river, length(parameters$code)), cex = ns_eff * 5., yaxt = 'n', ylab = "")
+plot(parameters$log_recession_coef , array(i_river, length(parameters$code)), cex = ns_eff * 5., yaxt = 'n', ylab = "")
+plot(parameters$stor_cap           , array(i_river, length(parameters$code)), cex = ns_eff * 5., yaxt = 'n', ylab = "")
 
 }
 
