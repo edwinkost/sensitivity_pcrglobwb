c25114cb87547f4be4422f6f45b0bb681e451370
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-10-20 00:09:00 +0200

    4th selection

--- 2015_10_XX/check_model_performance_with_selection_final_4th_select.R
+++ 2015_10_XX/check_model_performance_with_selection_final_4th_select.R
@@ -118,11 +118,8 @@ parameters = parameters[which(parameters$log_ksat == -0.50), ]
 # stor_cap should be at least 1.00
 parameters = parameters[which(parameters$stor_cap >= 1.00), ]
 
-# stor_cap should be 1.25
-parameters = parameters[which(parameters$stor_cap == 1.25), ]
-
-#~ # min_soil_depth_frac should be 0.75
-#~ parameters = parameters[which(parameters$min_soil_depth_frac == 0.75), ]
+# min_soil_depth_frac should be 0.75
+parameters = parameters[which(parameters$min_soil_depth_frac == 0.75), ]
 
 #~ # log_recession_coef should be -0.50
 #~ parameters = parameters[which(parameters$log_recession_coef == -0.50), ]
