40355e14fe3589dfbaa0f0210ad25abdff3677f1
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2015-10-19 19:23:09 +0200

    Lanjutkan.

--- 2015_10_XX/check_model_performance_with_selection_final.R
+++ 2015_10_XX/check_model_performance_with_selection_final.R
@@ -104,8 +104,8 @@ parameters = parameters[which(parameters$avg_runoff > 35000), ]
 parameters = parameters[which(parameters$avg_runoff < 50000), ]
 
 # average recharge should be between 15000 and 25000 km3/year
-parameters = parameters[which(parameters$avg_recharge > 15000), ]
-parameters = parameters[which(parameters$avg_recharge < 25000), ]
+parameters = parameters[which(parameters$groundwater_recharge > 15000), ]
+parameters = parameters[which(parameters$groundwater_recharge < 25000), ]
 
 # average evaporation should be above 55000 km3/year
 parameters = parameters[which(parameters$avg_evaporation > 55000), ]
@@ -127,112 +127,112 @@ file_name = paste("scatter_plot_parameters_", file_name_selection, ".pdf", sep =
 pdf(file_name, width=10, height=12, bg = "white")
 par(mfrow=c(4,4), mar=c(4,4,2,4))
 
-plot(parameters$min_soil_depth_frac, parameters$avg_evaporation)
+plot(parameters$min_soil_depth_frac, parameters$avg_evaporation, xlim = c(0.5, 1.5), ylim = c(30000, 80000))
 abline(h = evaporation_ref)
-plot(parameters$log_ksat           , parameters$avg_evaporation)
+plot(parameters$log_ksat           , parameters$avg_evaporation, xlim = c(-1, 1), ylim = c(30000, 80000))
 abline(h = evaporation_ref)
-plot(parameters$log_recession_coef , parameters$avg_evaporation)
+plot(parameters$log_recession_coef , parameters$avg_evaporation, xlim = c(-1, 1), ylim = c(30000, 80000))
 abline(h = evaporation_ref)
-plot(parameters$stor_cap           , parameters$avg_evaporation)
+plot(parameters$stor_cap           , parameters$avg_evaporation, xlim = c(0.5, 1.5), ylim = c(30000, 80000))
 abline(h = evaporation_ref)
 
-plot(parameters$min_soil_depth_frac, parameters$avg_runoff)
+plot(parameters$min_soil_depth_frac, parameters$avg_runoff, xlim = c(0.5, 1.5), ylim = c(25000, 75000))
 abline(h = runoff_ref)                                    
-plot(parameters$log_ksat           , parameters$avg_runoff)
+plot(parameters$log_ksat           , parameters$avg_runoff, xlim = c(-1, 1), ylim = c(25000, 75000))
 abline(h = runoff_ref)                                    
-plot(parameters$log_recession_coef , parameters$avg_runoff)
+plot(parameters$log_recession_coef , parameters$avg_runoff, xlim = c(-1, 1), ylim = c(25000, 75000))
 abline(h = runoff_ref)                                    
-plot(parameters$stor_cap           , parameters$avg_runoff)
+plot(parameters$stor_cap           , parameters$avg_runoff, xlim = c(0.5, 1.5), ylim = c(25000, 75000))
 abline(h = runoff_ref)
 
-plot(parameters$min_soil_depth_frac, parameters$avg_baseflow)
+plot(parameters$min_soil_depth_frac, parameters$avg_baseflow, xlim = c(0.5, 1.5), ylim = c(5000, 55000))
 abline(h = baseflow_ref)                                    
-plot(parameters$log_ksat           , parameters$avg_baseflow)
+plot(parameters$log_ksat           , parameters$avg_baseflow, xlim = c(-1, 1), ylim = c(5000, 55000))
 abline(h = baseflow_ref)                                    
-plot(parameters$log_recession_coef , parameters$avg_baseflow)
+plot(parameters$log_recession_coef , parameters$avg_baseflow, xlim = c(-1, 1), ylim = c(5000, 55000))
 abline(h = baseflow_ref)                                    
-plot(parameters$stor_cap           , parameters$avg_baseflow)
+plot(parameters$stor_cap           , parameters$avg_baseflow, xlim = c(0.5, 1.5), ylim = c(5000, 55000))
 abline(h = baseflow_ref)
 
-plot(parameters$min_soil_depth_frac, parameters$avg_groundwater_recharge)
+plot(parameters$min_soil_depth_frac, parameters$avg_groundwater_recharge, xlim = c(0.5, 1.5), ylim = c(5000, 55000))
 abline(h = recharge_ref)                                                
-plot(parameters$log_ksat           , parameters$avg_groundwater_recharge)
+plot(parameters$log_ksat           , parameters$avg_groundwater_recharge, xlim = c(-1, 1), ylim = c(5000, 55000))
 abline(h = recharge_ref)                                                
-plot(parameters$log_recession_coef , parameters$avg_groundwater_recharge)
+plot(parameters$log_recession_coef , parameters$avg_groundwater_recharge, xlim = c(-1, 1), ylim = c(5000, 55000))
 abline(h = recharge_ref)                                                
-plot(parameters$stor_cap           , parameters$avg_groundwater_recharge)
+plot(parameters$stor_cap           , parameters$avg_groundwater_recharge, xlim = c(0.5, 1.5), ylim = c(5000, 55000))
 abline(h = recharge_ref)
 
 dev.off()
 ########################################################################################################################
 
 
-#~ 
-#~ #############################################################################################################################
-#~ # get the list of all rivers/stations used
-#~ file_for_list_of_rivers = paste(main_path, "code__a__", as.character(0), "/analysis/monthly_discharge/summary.txt", sep = "")
-#~ performance_table = read.table(file_for_list_of_rivers, sep=";", header = T)
-#~ 
-#~ # select rivers with number of months >= 12
-#~ river = performance_table[which(performance_table$num_of_month_pairs >= 12), ]
-#~ 
-#~ # sort river based on grdc catchment area and use only the river name
-#~ river = as.character(river$river_name[order(-river$grdc_catchment_area_in_km2)])
-#~ #############################################################################################################################
-#~ 
-#~ 
-#~ # loop through all model performances:
-#~ performance_indi = c("ns_efficiency", "ns_efficiency_log", "kge_2009", "kge_2012", "R2")
-#~ 
-#~ for (i_indi in seq(1, length(performance_indi), 1) {
-#~ 
-#~ performance_used = performance_indi[i_indi]
-#~ performance_colm = which(names(performance_table) == performance_used)
-#~ 
-#~ file_name_indi = paste(performance_used, file_name_selection, ".pdf", sep = "")
-#~ 
-#~ pdf(file_name_indi, width = 30, height = 1.2 * length(river), bg = "white")
-#~ par(mfrow=c(length(river), ), mar=c(1,1,1,1))
-#~ 
-#~ # loop through all rivers to get model performances from all runs and their corresponding model parameters
-for (i_river in seq(1, length(river), 1)) {
-#~ for (i_river in seq(1, 5, 1)) {
-#~ 
-#~ # initiate model performance indicator (all NA)
-#~ indicator_value = array(NA, length(parameters$code))
-#~ 
-#~ # loop through all model runs
-#~ for (i_code in seq(1, length(parameters$code), 1)) {
-#~ 
-#~ # open/read file 
-#~ file_name = paste(main_path, "code__a__", as.character(i_code-1), "/analysis/monthly_discharge/summary.txt", sep = "")
-#~ print(file_name)
-#~ performance_table = read.table(file_name, sep=";", header = T)
-#~ 
-#~ # model performance indicators
-#~ indicator_value[[i_code] = performance_table[which(performance_table$river_name == river[i_river], performance_colm)]
-#~ 
-#~ # set minimum value of model performance indicator
-#~ indicator_value[[i_code][which(indicator_value[[i_code] < 0.0)] = 0.0
-#~ 
-#~ # print to indicate progress
-#~ print("")
-#~ print("Processing ...")
-#~ print(file_name)
-#~ print(river[i_river])
-#~ print(performance_used)
-#~ print("")
-#~ 
-#~ }
-#~ 
-#~ plot(c(parameters$min_soil_depth_frac, 10), array(i_river, length(parameters$code) + 1), cex = c(indicator_value, 1) * 7., yaxt = 'n', ylab = "", ylim = c(i_river - 0.5, i_river + 0.5), xlim = c(min(parameters$min_soil_depth_frac), max(parameters$min_soil_depth_frac)))
-#~ text(mean(parameters$min_soil_depth_frac), i_river + 0.25, labels = as.character(river[i_river]), cex = 1.5)
-#~ plot(c(parameters$log_ksat           , 10), array(i_river, length(parameters$code) + 1), cex = c(indicator_value, 1) * 7., yaxt = 'n', ylab = "", ylim = c(i_river - 0.5, i_river + 0.5), xlim = c(min(parameters$log_ksat           ), max(parameters$log_ksat           )))
-#~ plot(c(parameters$log_recession_coef , 10), array(i_river, length(parameters$code) + 1), cex = c(indicator_value, 1) * 7., yaxt = 'n', ylab = "", ylim = c(i_river - 0.5, i_river + 0.5), xlim = c(min(parameters$log_recession_coef ), max(parameters$log_recession_coef )))
-#~ plot(c(parameters$stor_cap           , 10), array(i_river, length(parameters$code) + 1), cex = c(indicator_value, 1) * 7., yaxt = 'n', ylab = "", ylim = c(i_river - 0.5, i_river + 0.5), xlim = c(min(parameters$stor_cap           ), max(parameters$stor_cap           )))
-#~ 
-#~ }
-#~ 
-#~ dev.off()
-#~ 
-#~ }
+
+#############################################################################################################################
+# get the list of all rivers/stations used
+file_for_list_of_rivers = paste(main_path, "code__a__", as.character(0), "/analysis/monthly_discharge/summary.txt", sep = "")
+performance_table = read.table(file_for_list_of_rivers, sep=";", header = T)
+
+# select rivers with number of months >= 12
+river = performance_table[which(performance_table$num_of_month_pairs >= 12), ]
+
+# sort river based on grdc catchment area and use only the river name
+river = as.character(river$river_name[order(-river$grdc_catchment_area_in_km2)])
+#############################################################################################################################
+
+
+# loop through all model performances:
+performance_indi = c("ns_efficiency", "ns_efficiency_log", "kge_2009", "kge_2012", "R2")
+
+for (i_indi in seq(1, length(performance_indi), 1) {
+
+performance_used = performance_indi[i_indi]
+performance_colm = which(names(performance_table) == performance_used)
+
+file_name_indi = paste(performance_used, file_name_selection, ".pdf", sep = "")
+
+pdf(file_name_indi, width = 30, height = 1.2 * length(river), bg = "white")
+par(mfrow=c(length(river), ), mar=c(1,1,1,1))
+
+# loop through all rivers to get model performances from all runs and their corresponding model parameters
+#~ for (i_river in seq(1, length(river), 1)) {
+for (i_river in seq(1, 5, 1)) {
+
+# initiate model performance indicator (all NA)
+indicator_value = array(NA, length(parameters$code))
+
+# loop through all model runs
+for (i_code in seq(1, length(parameters$code), 1)) {
+
+# open/read file 
+file_name = paste(main_path, "code__a__", as.character(i_code-1), "/analysis/monthly_discharge/summary.txt", sep = "")
+print(file_name)
+performance_table = read.table(file_name, sep=";", header = T)
+
+# model performance indicators
+indicator_value[[i_code] = performance_table[which(performance_table$river_name == river[i_river], performance_colm)]
+
+# set minimum value of model performance indicator
+indicator_value[[i_code][which(indicator_value[[i_code] < 0.0)] = 0.0
+
+# print to indicate progress
+print("")
+print("Processing ...")
+print(file_name)
+print(river[i_river])
+print(performance_used)
+print("")
+
+}
+
+plot(c(parameters$min_soil_depth_frac, 10), array(i_river, length(parameters$code) + 1), cex = c(indicator_value, 1) * 7., yaxt = 'n', ylab = "", ylim = c(i_river - 0.5, i_river + 0.5), xlim = c(min(parameters$min_soil_depth_frac), max(parameters$min_soil_depth_frac)))
+text(mean(parameters$min_soil_depth_frac), i_river + 0.25, labels = as.character(river[i_river]), cex = 1.5)
+plot(c(parameters$log_ksat           , 10), array(i_river, length(parameters$code) + 1), cex = c(indicator_value, 1) * 7., yaxt = 'n', ylab = "", ylim = c(i_river - 0.5, i_river + 0.5), xlim = c(min(parameters$log_ksat           ), max(parameters$log_ksat           )))
+plot(c(parameters$log_recession_coef , 10), array(i_river, length(parameters$code) + 1), cex = c(indicator_value, 1) * 7., yaxt = 'n', ylab = "", ylim = c(i_river - 0.5, i_river + 0.5), xlim = c(min(parameters$log_recession_coef ), max(parameters$log_recession_coef )))
+plot(c(parameters$stor_cap           , 10), array(i_river, length(parameters$code) + 1), cex = c(indicator_value, 1) * 7., yaxt = 'n', ylab = "", ylim = c(i_river - 0.5, i_river + 0.5), xlim = c(min(parameters$stor_cap           ), max(parameters$stor_cap           )))
+
+}
+
+dev.off()
+
+}
