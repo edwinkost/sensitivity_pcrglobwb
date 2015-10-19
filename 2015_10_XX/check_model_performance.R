2afa18f03681319d9bb23e9a85222c468492abf9
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-10-19 09:17:39 +0200

    Using symbols.

--- 2015_10_XX/check_model_performance.R
+++ 2015_10_XX/check_model_performance.R
@@ -42,7 +42,7 @@ river = river[which(river$num_of_month_pairs > 12), ]
 river = as.character(river$river_name[order(-river$grdc_catchment_area_in_km2)])
 
 
-pdf("0Rtest.pdf", width=30, height=length(river), bg = "white")
+pdf("0Rtest.pdf", width = 30, height=length(river), bg = "white")
 par(mfrow=c(length(river), 4), mar=c(1,1,1,1))
 
 # loop through all rivers to get model performances from all runs and their corresponding model parameters
@@ -83,12 +83,6 @@ kge_2009[i_code][which(kge_2009[i_code] < 0.0)] = 0.0
 kge_2012[i_code][which(kge_2012[i_code] < 0.0)] = 0.0
 R2[i_code][which(R2[i_code] < 0.0)] = 0.0      
 
-ns_eff[i_code]   = 0.0002 * ns_eff[i_code]  
-ns_log[i_code]   = 0.0002 * ns_log[i_code]  
-kge_2009[i_code] = 0.0002 * kge_2009[i_code]
-kge_2012[i_code] = 0.0002 * kge_2012[i_code]
-R2[i_code]       = 0.0002 * R2[i_code]      
-
 }
 
 #~ plot(parameters$min_soil_depth_frac, array(i_river, length(parameters$code)), cex = ns_eff * 7., yaxt = 'n', ylab = substr(as.character(river[i_river]), 1, 5), ylim = c(i_river - 0.5, i_river + 0.5))
@@ -122,11 +116,18 @@ R2[i_code]       = 0.0002 * R2[i_code]
 #~ plot(parameters$stor_cap           , array(i_river, length(parameters$code)), cex = R2 * 7., yaxt = 'n', ylab = "")
 
 
-symbols(parameters$min_soil_depth_frac, array(i_river, length(parameters$code)), circles = kge_2009, yaxt = 'n', ylab = "", ylim = c(i_river - 0.5, i_river + 0.5), bg=NULL, fg="black")
-text(mean(parameters$min_soil_depth_frac), i_river + 0.25, labels = as.character(river[i_river]), cex = 1.5)
-symbols(parameters$log_ksat           , array(i_river, length(parameters$code)), circles = kge_2009, yaxt = 'n', ylab = "", ylim = c(i_river - 0.5, i_river + 0.5), bg=NULL, fg="black")
-symbols(parameters$log_recession_coef , array(i_river, length(parameters$code)), circles = kge_2009, yaxt = 'n', ylab = "", ylim = c(i_river - 0.5, i_river + 0.5), bg=NULL, fg="black")
-symbols(parameters$stor_cap           , array(i_river, length(parameters$code)), circles = kge_2009, yaxt = 'n', ylab = "", ylim = c(i_river - 0.5, i_river + 0.5), bg=NULL, fg="black")
+#~ symbols(parameters$min_soil_depth_frac, array(i_river, length(parameters$code)), circles = kge_2009, yaxt = 'n', ylab = "", ylim = c(i_river - 0.5, i_river + 0.5), bg=NULL, fg="black")
+#~ text(mean(parameters$min_soil_depth_frac), i_river + 0.25, labels = as.character(river[i_river]), cex = 1.5)
+#~ symbols(parameters$log_ksat           , array(i_river, length(parameters$code)), circles = kge_2009, yaxt = 'n', ylab = "", ylim = c(i_river - 0.5, i_river + 0.5), bg=NULL, fg="black")
+#~ symbols(parameters$log_recession_coef , array(i_river, length(parameters$code)), circles = kge_2009, yaxt = 'n', ylab = "", ylim = c(i_river - 0.5, i_river + 0.5), bg=NULL, fg="black")
+#~ symbols(parameters$stor_cap           , array(i_river, length(parameters$code)), circles = kge_2009, yaxt = 'n', ylab = "", ylim = c(i_river - 0.5, i_river + 0.5), bg=NULL, fg="black")
+
+
+plot(parameters$min_soil_depth_frac, kge_2009, ylab = "", ylim = c(0, 1.5), bg=NULL, fg="black")
+text(mean(parameters$min_soil_depth_frac), 1.25, labels = as.character(river[i_river]), cex = 1.5)
+plot(parameters$log_ksat           , kge_2009, ylab = "", ylim = c(0, 1.5), bg=NULL, fg="black")
+plot(parameters$log_recession_coef , kge_2009, ylab = "", ylim = c(0, 1.5), bg=NULL, fg="black")
+plot(parameters$stor_cap           , kge_2009, ylab = "", ylim = c(0, 1.5), bg=NULL, fg="black")
 
 
 }
