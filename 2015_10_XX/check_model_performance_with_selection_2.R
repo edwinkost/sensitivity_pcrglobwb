fa6d2cb719969510382f8f6e61a5cbdee4c17de3
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-10-19 10:19:16 +0200

    Using symbols.

--- 2015_10_XX/check_model_performance_with_selection_2.R
+++ 2015_10_XX/check_model_performance_with_selection_2.R
@@ -28,6 +28,53 @@ parameters[,1] <- as.character(parameters[,1])
 parameters[,2:ncol(parameters)] <- lapply(parameters[,2:ncol(parameters)], as.numeric)
 ########################################################################################################################
 
+
+########################################################################################################################
+# make table of average values 
+
+# main path
+main_path = "~/github/edwinkost/sensitivity_pcrglobwb/2015_10_XX/summary_0to674/summary_"
+
+average_values = data.frame()
+
+for( i_run in seq(0,674,1)) {
+
+# open the log_summary file
+file_name = paste(main_path , as.character(i_run), ".sum", sep = "")
+print(file_name)
+
+# scenario_code
+code = paste("code__a__", as.character(i_run), sep = "")
+
+# open the log_summary file
+summary_table = read.table(file_name, sep = ";", header = T)
+
+avg_evaporation          = mean(summary_table$evaporation         )
+avg_runoff               = mean(summary_table$runoff              )
+avg_baseflow             = mean(summary_table$baseflow            )        
+avg_groundwater_recharge = mean(summary_table$groundwater_recharge)
+
+average_values = rbind(average_values, 
+                       cbind(code, 
+                             avg_evaporation,         
+                             avg_runoff,              
+                             avg_baseflow,            
+                             avg_groundwater_recharge))
+}
+
+# names of all columns
+names(average_values)[1] <- "code"
+names(average_values)[2] <- "avg_evaporation"        
+names(average_values)[3] <- "avg_runoff"             
+names(average_values)[4] <- "avg_baseflow"           
+names(average_values)[5] <- "avg_groundwater_recharge"
+
+# make sure formats are correct
+average_values[,1] <- as.character(average_values[,1])
+average_values[,2:ncol(average_values)] <- lapply(average_values[,2:ncol(average_values)], as.character)
+average_values[,2:ncol(average_values)] <- lapply(average_values[,2:ncol(average_values)], as.numeric)
+########################################################################################################################
+
 # merge two data frame
 parameters_ori = parameters
 parameters = merge(parameters, average_values, by = "code")
