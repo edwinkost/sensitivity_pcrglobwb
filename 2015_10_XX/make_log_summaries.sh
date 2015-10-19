61060f4daa6658c6851495c472b4b8015d20b50d
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2015-10-19 16:44:31 +0200

    Summarizing the results from the runs 675 to 1124.

--- 2015_10_XX/make_log_summaries.sh
+++ 2015_10_XX/make_log_summaries.sh
@@ -1,4 +1,4 @@
-for i in {0..674}
+for i in {675..1124}
 do
    echo "Processing the run $i."
    python ../make_log.summary.py /scratch-shared/edwin/sensitivity_analysis/2015_10_XX/code__a__$i/log/*.log summary_$i.sum
