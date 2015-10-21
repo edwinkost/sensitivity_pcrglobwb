e74cf9fb3d8545b402a78aee6b900d3e562c249a
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-10-21 10:01:53 +0200

    Identify highest ns_log.

--- 2015_10_XX/find_the_best_runs.R
+++ 2015_10_XX/find_the_best_runs.R
@@ -1,11 +1,9 @@
 
-# NOT FINISH YET
-
 # packages needed and clear all available existing objects:
 rm(list=ls()); # ls()
 
 # global folder for all scenarios
-global_folder = "/scratch/edwin/30min_22_jun_2015/rerun_for_iwmi/calibration_27_june_2015/"
+global_folder = "/scratch-shared/edwin/sensitivity_analysis/2015_10_XX/"
 
 first_run_to_be_analyzed = TRUE
 for (i in seq(0,1124,1)) {
@@ -14,54 +12,80 @@ for (i in seq(0,1124,1)) {
 run_code = paste("code__a__",i,sep="")
 
 # read table containing discharge analysis
-discharge_table_file =paste(global_folder, run_code,"/analysis/calibration/monthly_discharge/summary.txt",sep="") 
+discharge_table_file = paste(global_folder, run_code,"/analysis/monthly_discharge/summary.txt",sep="") 
 discharge_table = read.table(discharge_table_file, header=T, sep= ";")
 
+
+######################################################################################################
 # calculate performance values
-source("calculate_performance.R")
+#
+# - using ns_efficiency_log
+performance_per_river = discharge_table$ns_efficiency_log
+
+# - set minimum value
+min_value = 0.00
+performance_per_river[which(performance_per_river < min_value)] = 0.0
+
+# - average performance value
+performance = mean(performance_per_river, na.rm = T)
+print(performance)
+
+#
+######################################################################################################
+
+
 
 if (first_run_to_be_analyzed == TRUE) {
-summary = cbind(run_code, average_ns_discharge, baseflow_deviation, general_performance)
+summary = cbind(run_code, performance)
 first_run_to_be_analyzed = FALSE
 } else {
 summary = rbind(summary, 
-          cbind(run_code, average_ns_discharge, baseflow_deviation, general_performance))
+          cbind(run_code, performance))
 }
 }
 
-# convert summary table to data frame
+
+######################################################################################################
+# convert performance summary table to data frame
 summary_df = data.frame(summary)
 summary_df$run_code             = as.character(summary_df$run_code)
-summary_df$baseflow_deviation   = as.numeric(as.character(summary_df$baseflow_deviation))
-summary_df$average_ns_discharge = as.numeric(as.character(summary_df$average_ns_discharge))
-summary_df$general_performance  = as.numeric(as.character(summary_df$general_performance))
+summary_df$performance          = as.numeric(as.character(summary_df$performance))
+######################################################################################################
 
+
+########################################################################################################################
 # merge it to parameters table
-parameter_table_1st = read.table("/home/sutan101/github/edwinkost/calibration_log_IWMI/runs_for_IWMI_calibration_code_AA/table_27_june_2014_speedy.txt",header=TRUE)
-parameter_table_2nd = read.table("/home/sutan101/github/edwinkost/calibration_log_IWMI/runs_for_IWMI_calibration_code_AA/table_27_june_2014_speedy_2.txt",header=TRUE)
-parameter_table_3rd = read.table("/home/sutan101/github/edwinkost/calibration_log_IWMI/runs_for_IWMI_calibration_code_AA/table_30_june_2014_cartesius.txt",header=TRUE)
-reference_run = c("code__a__0",1.0,0.0,0.0,1.0)
-#
-parameter_table = rbind(reference_run, parameter_table_1st, parameter_table_2nd, parameter_table_3rd) 
-parameter_table[ ,1] <- as.character(parameter_table[,1])
-parameter_table[1,1] <- "code__a__0"
-parameter_table[ ,2] <- as.numeric(parameter_table[,2])
-parameter_table[ ,3] <- as.numeric(parameter_table[,3])
-parameter_table[ ,4] <- as.numeric(parameter_table[,4])
-names(parameter_table)[1] <- "run_code"
-names(parameter_table)[2] <- "min_soil_depth_frac"
-names(parameter_table)[3] <- "log_ksat"
-names(parameter_table)[4] <- "log_recession_coef"
-names(parameter_table)[5] <- "degree_day_factor"
-#
-final_table = merge(parameter_table, summary_df, by = "run_code")
+parameters = data.frame()
+parameters = rbind(parameters, read.table("../table_05_october_2015_speedy_and_rapid.txt", header=T)[1:5])            
+parameters = rbind(parameters, read.table("../table_05_october_2015_cartesius.txt", header=T)[1:5])                   
+parameters = rbind(parameters, read.table("../table_06_october_2015_cartesius_and_speedy_rapid.txt", header=T)[1:5])
+parameters = rbind(parameters, read.table("../table_08_october_2015_cartesius.txt", header=T)[1:5])
+parameters = rbind(parameters, read.table("../table_12_october_2015_cartesius.txt", header=T)[1:5])
+parameters = rbind(parameters, read.table("../table_14_october_2015_cartesius.txt", header=T)[1:5])
+parameters = rbind(parameters, read.table("../table_15_october_2015_cartesius.txt", header=T)[1:5])
+
+# the first column is character
+parameters[,1] = as.character(parameters[,1])
+
+# reference run  
+parameters = rbind(parameters, c("code__a__0", 1.0, 0.0, 0.0, 1.0))
+
+# the first column is code
+names(parameters)[1] <- "run_code"
+
+# make sure formats are correct
+parameters[,1] <- as.character(parameters[,1])
+parameters[,2:ncol(parameters)] <- lapply(parameters[,2:ncol(parameters)], as.numeric)
+
+final_table = merge(parameters, summary_df, by = "run_code")
+
+########################################################################################################################
+
 
 # order it based on performance values
-#~ final_table = final_table[order(final_table$general_performance), ]
-final_table = final_table[order(-final_table$general_performance), ]
-final_table[1:40,]
+final_table = final_table[order(-final_table$performance), ]
+final_table[1:20,]
 
-write.table(final_table,file = "scatterplot_july_2015.txt", col.names=TRUE,row.names=FALSE,sep=";")
 
 
 
