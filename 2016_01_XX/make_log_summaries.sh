6847b333e0b9ef83c5724cb97fc673ca76584ed7
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2016-01-05 11:35:29 +0100

    Add making a log summary script.

--- 2016_01_XX/make_log_summaries.sh
+++ 2016_01_XX/make_log_summaries.sh
@@ -5,5 +5,5 @@ do
    OUT=/scratch-shared/edwin/sensitivity_analysis_non_natural/2016_01_XX/code__a__
    echo "Processing the run $i."
    echo "python make_log.summary.py $OUT$i/log/*.log $OUT$i/log/summary_$i.sum"
-   #~ python make_log.summary.py $OUT$i/log/*.log $OUT$i/log/summary_$i.sum
+   python make_log.summary.py $OUT$i/log/*.log $OUT$i/log/summary_$i.sum
 done
