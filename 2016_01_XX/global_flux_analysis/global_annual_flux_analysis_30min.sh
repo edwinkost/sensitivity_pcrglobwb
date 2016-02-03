914676dc366a3ab1085af0c035e778a7b038e2eb
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2016-02-03 21:36:57 +0100

    Analyzing global annual fluxes.

---
+++ 2016_01_XX/global_flux_analysis/global_annual_flux_analysis_30min.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+for i in {0..2}
+do
+   OUT=code__a__
+   echo "Processing the run $i."
+   echo "R -f global_annual_flux_analysis_30min.R $OUT$i"
+   #~ R -f global_annual_flux_analysis_30min.R $OUT$i
+   if ! ((i % 4))
+      echo "wait"         
+   fi
+done
+
+#~ R -f global_annual_flux_analysis_30min.R with_fossil_limit_and_with_pumping_limit 
+#~ R -f global_annual_flux_analysis_30min.R with_fossil_limit_and_without_pumping_limit
+#~ R -f global_annual_flux_analysis_30min.R without_fossil_limit_and_with_pumping_limit
+#~ R -f global_annual_flux_analysis_30min.R without_fossil_limit_and_without_pumping_limit
+
