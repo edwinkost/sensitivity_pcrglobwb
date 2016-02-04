da70b5bf75d12cd2749d5f1cba218c3870f83ee3
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2016-02-04 16:14:45 +0100

    Analyzing monthly discharge and annual baseflow.

--- 2016_01_XX/discharge_analysis_iwmi/00_analyzing_bash.py
+++ 2016_01_XX/discharge_analysis_iwmi/00_analyzing_bash.py
@@ -6,18 +6,21 @@ import sys
 
 # starting and last_run code
 sta_run = 0
-end_run = 224 
+end_run = 20 
 
 # maximum number of cores
-max_cores = 20
+max_cores = 5
 
 # making command lines
 cmd = ''
 for i_run in range(sta_run, end_run + 1):
 
-   cmd += "python 0_main_analyze_discharge.py " + "/scratch-shared/edwin/sensitivity_analysis_non_natural/2016_01_XX/code__a__" + str(i_run)
+   cmd += "python 0_main_analyze_discharge_IWMI.py " + "/projects/0/dfguu/users/edwin/30min_sensitivity_analysis_non_natural/2016_01_XX/code__a__" + str(i_run) + " iwmi_calibration"
    cmd = cmd +" & \n"
    
+   cmd += "python 0_main_analyze_discharge_IWMI.py " + "/projects/0/dfguu/users/edwin/30min_sensitivity_analysis_non_natural/2016_01_XX/code__a__" + str(i_run) + " iwmi_validation"
+   cmd = cmd +" & \n"
+
    if ((i_run + 1)%max_cores == 0) or (i_run == end_run): cmd = cmd + "wait \n"       
 
 print cmd
