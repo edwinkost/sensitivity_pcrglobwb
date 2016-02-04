9ca3de84d0835d47fa1bb9530da4dd4c147c5dbe
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2016-02-04 20:18:47 +0100

    Working to find the best run.

--- 2016_01_XX/make_log_summaries.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in {0..224}
-do
-   OUT=/scratch-shared/edwin/sensitivity_analysis_non_natural/2016_01_XX/code__a__
-   echo "Processing the run $i."
-   echo "python make_log.summary.py $OUT$i/log/*.log $OUT$i/log/summary_$i.sum"
-   python make_log.summary.py $OUT$i/log/*.log $OUT$i/log/summary_$i.sum
-done
