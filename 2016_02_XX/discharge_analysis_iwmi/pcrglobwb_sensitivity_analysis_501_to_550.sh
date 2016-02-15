f2ad57929dd250b456185ae9dc3090ab6f51e472
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2016-02-15 21:02:05 +0100

    Analyzing discharge for the 675 runs.

---
+++ 2016_02_XX/discharge_analysis_iwmi/pcrglobwb_sensitivity_analysis_501_to_550.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -t 24:00:00
+#SBATCH -p normal
+
+python 00_analyzing_bash.py 501 550
+
+
+
---
+++ 2016_02_XX/discharge_analysis_iwmi/pcrglobwb_sensitivity_analysis_651_to_674.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -t 24:00:00
+#SBATCH -p normal
+
+python 00_analyzing_bash.py 601 650
+
+
+
+
