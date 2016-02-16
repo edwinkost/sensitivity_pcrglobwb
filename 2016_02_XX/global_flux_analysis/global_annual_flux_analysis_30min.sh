d222624fce1beaf4d9229ad5881a31740e18b380
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2016-02-16 01:55:55 +0100

    Analyzing global fluxes for the 675 runs.

---
+++ 2016_02_XX/global_flux_analysis/global_annual_flux_analysis_30min.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+for i in {0..225}
+do
+   OUT=code__a__
+   echo "Processing the run $i."
+   echo "R -f global_annual_flux_analysis_30min.R $OUT$i &"
+   R -f global_annual_flux_analysis_30min.R $OUT$i &
+   if ! ((i % 4)); then
+      echo "wait"
+      wait         
+   fi
+done
+
+R -f global_annual_flux_analysis_30min.R with_fossil_limit_and_with_pumping_limit &
+R -f global_annual_flux_analysis_30min.R with_fossil_limit_and_without_pumping_limit &
+R -f global_annual_flux_analysis_30min.R without_fossil_limit_and_with_pumping_limit &
+R -f global_annual_flux_analysis_30min.R without_fossil_limit_and_without_pumping_limit &
+wait
+echo "all done"
+
