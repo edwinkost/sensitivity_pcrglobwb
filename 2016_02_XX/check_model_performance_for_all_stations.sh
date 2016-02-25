c6819e4711cbc5d5254b8359bda6300c0b80d7ee
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2016-02-25 18:05:37 +0100

    Continue the plotting.

---
+++ 2016_02_XX/check_model_performance_for_all_stations.sh
@@ -0,0 +1,6 @@
+
+R -f check_model_performance_for_all_stations.R "kge_2009" "validation" &
+R -f check_model_performance_for_all_stations.R "kge_2009" "calibration" &
+
+R -f check_model_performance_for_all_stations.R "one_min_bfdv" "validation" &
+R -f check_model_performance_for_all_stations.R "one_min_bfdv" "calibration" &
