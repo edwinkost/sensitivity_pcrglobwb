c41b9aea6c7f5463f87dae5eae48dc1cb8fdc910
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2016-02-25 14:41:24 +0100

    Continue the plotting.

--- 2016_02_XX/check_model_performance_for_all_stations.R
+++ 2016_02_XX/check_model_performance_for_all_stations.R
@@ -74,9 +74,9 @@ river_number = array(river_number, length(parameters$code))
 # river names (only for documentation purpose) 
 river_name = array(river[i_river], length(parameters$code))
 
-#~ # loop through all model runs
-for (i_code in seq(1, length(parameters$code), 1)) {
-#~ for (i_code in seq(1, 1, 1)) {
+# loop through all model runs
+#~ for (i_code in seq(1, length(parameters$code), 1)) {
+for (i_code in seq(1, 5, 1)) {
 
 # code for this run and their parameter/pre-factor values
 code               [i_code] = parameters$code               [i_code]
@@ -166,8 +166,8 @@ chart_for_log_ksat            <- chart_for_log_ksat            + geom_point(data
 chart_for_stor_cap            <- chart_for_stor_cap            + geom_point(data = table_for_this_river, aes(x = stor_cap,            y = river_number, size = kge_2009), shape = 21)
 chart_for_log_recession_coef  <- chart_for_log_recession_coef  + geom_point(data = table_for_this_river, aes(x = log_recession_coef,  y = river_number, size = kge_2009), shape = 21)
 
-# to the last column add river names
-chart_for_log_recession_coef  <- chart_for_log_recession_coef + annotate("text", x = 1.25, y = river_number, label = river_name[1], size = 0.1)
+# to the last column add river name
+chart_for_log_recession_coef  <- chart_for_log_recession_coef  + annotate("text", x = 1.25, y = river_number, label = river_name[1], size = 0.1)
 
 # plot for one_min_bfdv
 
