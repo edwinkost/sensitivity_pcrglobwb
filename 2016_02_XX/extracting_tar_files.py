e80ebc7fe42a30a5d90a1a77025bca57cad5938e
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2017-03-17 16:51:43 +0100

    extracting archive files

--- 2016_02_XX/extracting_tar_files.py
+++ 2016_02_XX/extracting_tar_files.py
@@ -27,3 +27,23 @@ for i in range(start_number, end_number + 1, 1):
 
 
 
+
+# python extracting_tar_files.py 0	 50  &
+# python extracting_tar_files.py 51	 100 &
+# python extracting_tar_files.py 101 150 &
+# python extracting_tar_files.py 151 200 &
+# python extracting_tar_files.py 201 250 &
+# python extracting_tar_files.py 251 300 &
+# python extracting_tar_files.py 301 350 &
+# python extracting_tar_files.py 351 400 &
+# python extracting_tar_files.py 401 450 &
+# python extracting_tar_files.py 451 500 &
+# python extracting_tar_files.py 501 550 &
+# python extracting_tar_files.py 551 600 &
+# python extracting_tar_files.py 601 650 &
+# python extracting_tar_files.py 651 674 &
+# wait
+
+
+
+
