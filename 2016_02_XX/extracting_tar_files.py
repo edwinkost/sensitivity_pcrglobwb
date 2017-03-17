8c8beb30d4c64423fc2ee04c1bc4d64c862f9be0
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2017-03-17 16:38:26 +0100

    extracting archive files

--- 2016_02_XX/extracting_tar_files.py
+++ 2016_02_XX/extracting_tar_files.py
@@ -12,7 +12,7 @@ end_number   = int(sys.argv[2])
 location_of_archive_files = "/scratch-shared/edwinwce/iwmi_runs/"
 front_file_name           = "code__a__"
 file_name_extension       = ".tar"
-target folder             = "/scratch-shared/edwinhs/iwmi_runs/"
+target_folder             = "/scratch-shared/edwinhs/iwmi_runs/"
 
 for i in range(start_number, end_number + 1, 1):
     # make the output folder
@@ -20,6 +20,6 @@ for i in range(start_number, end_number + 1, 1):
     if os.path.exists(pcrglobwb_output_folder): shutil.rmtree(pcrglobwb_output_folder)
     os.makedirs(pcrglobwb_output_folder)
     # untar to the output folder
-    cmd = "tar -xvf " + location_of_archive_files + front_file_name + str(i) + ".tar -C " + "code__a__" + str(i)
+    cmd = "tar -xvf " + location_of_archive_files + front_file_name + str(i) + ".tar -C " + target_folder + "code__a__" + str(i)
     print(cmd)
     os.system(cmd)
