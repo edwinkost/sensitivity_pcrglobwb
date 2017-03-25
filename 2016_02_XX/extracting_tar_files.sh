16086979a29b38398d780d11113693d624ed6285
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2017-03-25 10:35:34 +0100

    extracting tar files - redo and faster

--- 2016_02_XX/extracting_tar_files.sh
+++ 2016_02_XX/extracting_tar_files.sh
@@ -1,15 +1,30 @@
-python extracting_tar_files.py 0   50  &
-python extracting_tar_files.py 51  100 &
-python extracting_tar_files.py 101 150 &
-python extracting_tar_files.py 151 200 &
-python extracting_tar_files.py 201 250 &
-python extracting_tar_files.py 251 300 &
-python extracting_tar_files.py 301 350 &
-python extracting_tar_files.py 351 400 &
-python extracting_tar_files.py 401 450 &
-python extracting_tar_files.py 451 500 &
-python extracting_tar_files.py 501 550 &
-python extracting_tar_files.py 551 600 &
-python extracting_tar_files.py 601 650 &
-python extracting_tar_files.py 651 674 &
+python extracting_tar_files.py  0	1
+wait
+python extracting_tar_files.py  1	25  &
+python extracting_tar_files.py  26	50  &
+python extracting_tar_files.py  51	75  &
+python extracting_tar_files.py  76	100 &
+python extracting_tar_files.py  101	125 &
+python extracting_tar_files.py  126	150 &
+python extracting_tar_files.py  151	175 &
+python extracting_tar_files.py  176	200 &
+python extracting_tar_files.py  201	225 &
+python extracting_tar_files.py  226	250 &
+python extracting_tar_files.py  251	275 &
+python extracting_tar_files.py  276	300 &
+python extracting_tar_files.py  301	325 &
+python extracting_tar_files.py  326	350 &
+python extracting_tar_files.py  351	375 &
+python extracting_tar_files.py  376	400 &
+python extracting_tar_files.py  401	425 &
+python extracting_tar_files.py  426	450 &
+python extracting_tar_files.py  451	475 &
+python extracting_tar_files.py  476	500 &
+python extracting_tar_files.py  501	525 &
+python extracting_tar_files.py  526	550 &
+python extracting_tar_files.py  551	575 &
+python extracting_tar_files.py  576	600 &
+python extracting_tar_files.py  601	625 &
+python extracting_tar_files.py  626	650 &
+python extracting_tar_files.py  651	674 &
 wait
